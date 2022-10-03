41f41be6d70b7b3812f0a6e98cfcfe60a0c01ff9
LalithoRivera <e.rivera.z312@edu.utc.mx>
2022-10-03 10:58:57 -0500

    Trabajo de hoy

---
+++ 06Figuras/principal.java
@@ -0,0 +1,8 @@
+public class principal{
+
+    public static void main(String[] args){
+        //instancia de la clase
+        figura objfigura = new figura();
+        objfigura.menu();
+    }
+}
